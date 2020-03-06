61bd8c1e2a54ec762d2803f3c3b14093f7a17e25
Andre Skeene <halodre24@gmail.com>
2020-03-06 22:06:02 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+class Post
+  attr_accessor :name, :author, :title
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @author = author
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    if @author == nil
+      nil
+    else
+      @author.name
+    end
+  end
+end
